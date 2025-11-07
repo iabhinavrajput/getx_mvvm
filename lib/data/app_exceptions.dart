5924f0bfb15188fa57b837f16c1dbbc80743e417
iabhinavrajput <abhinavrajput207@gmail.com>
2025-11-07 22:24:21 +0530

    feat: add app exceptions; set SplashScreen

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,20 @@
+class AppExceptions implements Exception {
+  final _message;
+  final _prefix;
+  
+  AppExceptions([this._message, this._prefix]);
+
+  String toString() {
+    return '$_prefix$_message';
+  }
+}
+
+class InternetExceptions extends AppExceptions {
+  InternetExceptions([String ? message]) : super(message, 'No internet');
+}
+
+class TimeOutExceptions extends AppExceptions {
+  TimeOutExceptions([String ? message]) :super(message, 'Time out');
+}
+
+
