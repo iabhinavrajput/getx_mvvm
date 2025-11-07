a180b71ffdf5e114a6e443e587c22d9bd4cd894a
iabhinavrajput <abhinavrajput207@gmail.com>
2025-11-08 00:11:49 +0530

    feat(utils): add focus, toast/snackbar helpers and AppColors

---
+++ lib/res/colors/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color blackColor = Color(0xff00000);
+  static const Color whiteColor = Color(0xffffffff);
+}
---
+++ lib/utils/utils.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:getx_mvvm/res/colors/app_colors.dart';
+
+class Utils {
+  static void fieldFocusChange(
+    BuildContext context,
+    FocusNode current,
+    FocusNode nextFocus,
+  ) {
+    current.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  static toastMessage(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: AppColors.blackColor,
+      gravity: ToastGravity.BOTTOM,
+    );
+  }
+
+  static snackBar(String title, String message) {
+    Get.snackbar(title, message);
+  }
+}
